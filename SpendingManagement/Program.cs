02900da46184ad7d5b21f03518ac7eb81f352ab9
dawng278 <haidang27082005@gmail.com>
2026-03-11 13:54:00 +0700

    """ Initialize WinForms project structure and resources
    Set up initial project files for a Spending Management WinForms app, including Form1, resource files, and project configuration. Added references to Guna UI libraries and created basic entry point in Program.cs. Included designer and resx files for localization and resources.
    """

---
+++ SpendingManagement/Program.cs
@@ -0,0 +1,28 @@
+﻿using SpendingManagement.UI.Forms;
+using SpendingManagement.UI.Forms.Auth;
+
+namespace SpendingManagement
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // 1. Cài đặt hệ thống phải để ĐẦU TIÊN
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+
+            // 2. Sau đó mới khởi tạo cấu hình app
+            ApplicationConfiguration.Initialize();
+
+            // 3. Cuối cùng mới Run Form
+            //Application.Run(new LoginForm());
+            //Application.Run(new RegisterForm());
+            Application.Run(new BaseForm());
+        }
+    }
+}
